c0782bbba9eed9234582c89e7163fde9b1b783fc
iftekhar <princemahmood117@gmail.com>
2026-07-28 17:30:33 +0600

    python printing types differences

---
+++ task.py
@@ -0,0 +1,16 @@
+age : int = 11;
+print(type(age))
+
+car  = "Toyota"  #type = str
+print(type(age))
+
+
+base = 6
+height = 3
+area = (base*height)/2
+print("The area of the triangle is: " + str(area)) 
+
+base = 10
+height = 20
+area = (base*height)/2
+print(f"The area of triangle is : {area}")
