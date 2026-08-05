18c482e79fac4ffc9383464bf0bcf95d654961f2
iftekhar <princemahmood117@gmail.com>
2026-08-05 22:14:38 +0600

    string manipulation

---
+++ new/string.py
@@ -0,0 +1,16 @@
+test = "system"
+
+print(test.index('s'))
+
+
+message = "A kong string with a silly typo"
+new_message = message[0:2] + "l" + message[3:]
+
+
+print(new_message)
+
+
+message = "This is a new message"
+print(message)
+message = "And another one"
+print(message)
