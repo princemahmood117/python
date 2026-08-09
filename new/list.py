8cd339d2b0f3f473b6c41493517d27086de3b6d0
iftekhar <princemahmood117@gmail.com>
2026-08-09 23:46:10 +0600

    ..

--- new/list.py
+++ new/list.py
@@ -79,4 +79,4 @@ print(odd_numbers(5))  # Should print [1, 3, 5]
 print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
 print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
 print(odd_numbers(1))  # Should print [1]
-print(odd_numbers(-1)) # Should print []
+print(odd_numbers(-1)) # Should print []..............
